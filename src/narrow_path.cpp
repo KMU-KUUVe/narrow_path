aaa694c89632918f7a2ea842518ae2ca56c1ec38
Donghee Han <hdh7485@gmail.com>
2018-03-20 19:06:09 +0900

    Complete basic algorithm.

--- src/narrow_path.cpp
+++ src/narrow_path.cpp
@@ -16,18 +16,11 @@ using namespace std;
 
 ros::Publisher pub;
 
-#define CONST_STEER = 1500;
-#define CONST_SPEED = 1530;
+#define CONST_STEER 1500.0;
+#define CONST_SPEED 1520;
 
-/*double distance_from_car(geometry_msgs::Point p1, geometry_msgs::Point p2){
-	<<<<<<< HEAD
-	double gradient = (p1.x - p2.x) / (p1.y - p2.y);
-	double distance = abs(-gradient * p1.y + p1.x) / sqrt(1 + gradient * gradient);
-	return distance;
-	}*/
-
-bool cmp(obstacle_detector::CircleObstacle a, obstacle_detector::CircleObstacle b){
-		return a.center.x < b.center.x;
+bool cmp(const obstacle_detector::CircleObstacle a, const obstacle_detector::CircleObstacle b){
+		return (a.center.x < b.center.x);
 }
 
 void obstacle_cb(const obstacle_detector::Obstacles data) {
@@ -35,20 +28,31 @@ void obstacle_cb(const obstacle_detector::Obstacles data) {
 		vector<obstacle_detector::CircleObstacle> left_circles;
 		vector<obstacle_detector::CircleObstacle> right_circles;
 
-		cout << 1 << endl;
-
 		for(int i = 0; i < data.circles.size(); i++) {
-				if(data.circles[i].center.x > 0 && data.circles[i].center.y < 0){ //Right side
+				if(data.circles[i].center.x > -0.2 && data.circles[i].center.y < 0){ //Right side
 						right_circles.push_back(data.circles[i]);
 				}
-				else if(data.circles[i].center.x > 0 && data.circles[i].center.y > 0){ //Left side
+				else if(data.circles[i].center.x > -0.2 && data.circles[i].center.y > 0){ //Left side
 						left_circles.push_back(data.circles[i]);
 				}
 		}
 
-		cout << 2 << endl;
-
 		//push circle obstacles at right and left vector.
+		/*
+		cout << "right" << endl;
+		for(int i = 0; i < right_circles.size(); i++){
+				cout << right_circles[i].center << endl;
+		}
+		cout << "left" << endl;
+		for(int i = 0; i < left_circles.size(); i++){
+				cout << left_circles[i].center << endl;
+		}
+*/
+		if(right_circles.size() > 1) //check vector is empty.
+				sort(right_circles.begin(), right_circles.end(), cmp);
+		if(left_circles.size() > 1) //check vector is empty.
+				sort(left_circles.begin(), left_circles.end(), cmp);
+
 		cout << "right" << endl;
 		for(int i = 0; i < right_circles.size(); i++){
 				cout << right_circles[i].center << endl;
@@ -58,28 +62,18 @@ void obstacle_cb(const obstacle_detector::Obstacles data) {
 				cout << left_circles[i].center << endl;
 		}
 
-		if(right_circles.size() > 0) //check vector is empty.
-				sort(right_circles.begin(), right_circles.end(), cmp);
-		if(left_circles.size() > 0) //check vector is empty.
-				sort(left_circles.begin(), right_circles.end(), cmp);
+		int steer = 1500; 
+		int speed = 1500;
 
-		cout << 3 << endl;
+		if(left_circles.size() >= 2 && right_circles.size() >= 2){
+				double mean_point_y = right_circles[0].center.y + right_circles[1].center.y + left_circles[0].center.y + left_circles[1].center.y; 
+				//		double mean_point_y = right_circles[0].center.x + right_circles[1].center.x + left_circles[0].center.x + left_circles[1].center.x; 
 
-		for(int i = 0; i < right_circles.size(); i++){
-				cout << right_circles[i].center << endl;
+				steer = int(mean_point_y * -400.0) + CONST_STEER;
+				speed = CONST_SPEED;
+				cout << mean_point_y << " " << steer << " " << speed << endl;
 		}
-		for(int i = 0; i < left_circles.size(); i++){
-				cout << left_circles[i].center << endl;
-		}
-
-		cout << 4 << endl;
-
-		mean_point_x = right_circles[0].center.y + right_circles[1].center.y + left_circles[0].center.y + left_circles[1].center.y; 
-		//mean_point_y = right_circles[0].center.x + right_circles[1].center.x + left_circles[0].center.x + left_circles[1].center.x; 
-
-		steer = CONST_STEER + (mean_point_x * 200);
-		speed = CONST_SPEED;
-
+		std_msgs::String msg;
 		msg.data = std::to_string(steer) + "," + std::to_string(speed) + ",";	 
 		pub.publish(msg);
 }
