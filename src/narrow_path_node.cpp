520d826cc2d867e5eec48e25dbe5f0ec8395def7
Donghee Han <hdh7485@gmail.com>
2018-04-11 20:05:11 +0900

    Modify to class.

---
+++ src/narrow_path_node.cpp
@@ -0,0 +1,15 @@
+#include "narrow_path/narrow_path.h"
+#include <actionlib/server/simple_action_server.h>
+#include <action_with_smach/MissionPlannerAction.h>
+
+int main(int argc, char* argv[]) {
+	ros:: init(argc, argv, "narrow_path_node");
+
+	narrow_paht::NarrowPath node;
+	node.run();
+
+	ros::Subscriber sub = nh.subscribe("raw_obstacles", 1, obstacle_cb);
+	pub = nh.advertise<ackermann_msgs::AckermannDriveStamped> ("ackermann", 100);
+	//pub = nh.advertise<std_msgs::String>("write", 1000);
+	ros::spin();
+}
