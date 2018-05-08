c08803a1dec5544d5d7ff3def7b791e814127d0e
Donghee Han <hdh7485@gmail.com>
2018-05-08 13:42:21 +0900

    Fix s_path code when obstacles on one_side detected.

--- src/narrow_path_backup.cpp
+++ src/narrow_path_backup.cpp
@@ -64,6 +64,18 @@ void NarrowPath::obstacle_cb(const obstacle_detector::Obstacles data){
 		}
 	}
 
+	/* /////////////////////////////original code/////////////////////////////
+	   for(int i = 0; i < data.circles.size(); i++) {
+	   if(data.circles[i].center.x > -0.2 && data.circles[i].center.y < 0){ //Right side
+	   right_circles.push_back(data.circles[i]);
+	   }
+	   else if(data.circles[i].center.x > -0.2 && data.circles[i].center.y > 0){ //Left side
+	   left_circles.push_back(data.circles[i]);
+	   }
+	   }
+
+*/
+
 	//rubber cone sorting
 	if(right_circles.size() > 1) //check vector is empty.
 		sort(right_circles.begin(), right_circles.end(), cmp);
@@ -113,25 +125,18 @@ void NarrowPath::run(){
 
 		if(left_circles.size() >= 1 && right_circles.size() >= 1){
 			end_flag = false;
-
-			if(abs(right_circles[0].center.x-left_circles[0].center.x) > 0.5){
-				mean_point_y = 0;
-				ROS_INFO("one_side detect")
+			if(right_circles.size() >=2){
+				mean_point_right_y = (right_circles[0].center.y + right_circles[1].center.y) / 2; 
 			}
-			else{
-				if(right_circles.size() >=2){
-					mean_point_right_y = (right_circles[0].center.y + right_circles[1].center.y) / 2; 
-				}
-				else {
-					mean_point_right_y = right_circles[0].center.y; 
-				}
+			else {
+				mean_point_right_y = right_circles[0].center.y; 
+			}
 
-				if(left_circles.size() >=2){
-					mean_point_left_y = (left_circles[0].center.y + left_circles[1].center.y) / 2; 
-				}
-				else{ 
-					mean_point_left_y = left_circles[0].center.y; 
-				}
+			if(left_circles.size() >=2){
+				mean_point_left_y = (left_circles[0].center.y + left_circles[1].center.y) / 2; 
+			}
+			else{ 
+				mean_point_left_y = left_circles[0].center.y; 
 			}
 
 			mean_point_y = mean_point_right_y + mean_point_left_y;
